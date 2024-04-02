59b2af5b7fc6ac2b8e95b978f2811842b03d0957
Miguel Rico <mricofernandez@safareyes.es>
2024-04-02 17:04:41 +0200

    🗃️ Add ORM models

---
+++ api/models/notification_type.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class NotificationType(models.Model):
+    """
+    Modelo que representa el tipo de notificación.
+    """
+
+    tittle = models.CharField(Null=True, blank=True, max_length=100)
+    icon = models.CharField()
+    description = models.TextField(Null=True, blank=True)
+
+    def __str__(self):
+        return self.tittle
