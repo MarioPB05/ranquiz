7789daa7a6e90e11fc4e3c6d29d448fddb756548
David <dperezfernandez@safareyes.es>
2024-04-02 17:32:30 +0200

    🗃️ Update list model

--- api/models/goal.py
+++ api/models/goal.py
@@ -5,7 +5,7 @@ class Goal(models.Model):
     """
     Modelo que representa el valor objetivo y la recompensa de las misiones
     """
-    id_type = models.ForeignKey('GoalType', on_delete=models.CASCADE)
+    id_type = models.ForeignKey('GoalType', on_delete=models.DO_NOTHING, related_name='goals')
     value = models.IntegerField()
     reward = models.IntegerField()
 
