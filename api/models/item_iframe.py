87760775825bdb82a56ee73b2cfb957266f29160
Mario <marioperdiguero37@gmail.com>
2024-04-02 17:56:24 +0200

    🗃️ Add ORM models

---
+++ api/models/item_iframe.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class ItemIframe(models.Model):
+    """
+    Modelo que representa un iframe de un item.
+    """
+    url = models.URLField()
+    item = models.ForeignKey('Item', on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return f'{self.item.name} - {self.url}'
---
+++ api/models/item_order.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class ItemOrder(models.Model):
+    """
+    Modelo que representa el orden de los items en una lista de una determinada jugada.
+    """
+    item = models.ForeignKey('Item', on_delete=models.DO_NOTHING)
+    answer = models.ForeignKey('ListAnswer', on_delete=models.DO_NOTHING)
+    order = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'{self.item.name} - {self.answer.id}'
