68db51e920178edc55da192cbfb189d3979e5aa5
Jorge Arias <jariasmartin@safareyes.es>
2024-04-02 17:04:19 +0200

    🗃️ Add ORM models

---
+++ api/models/award.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Award(models.Model):
+    """
+    Modelo que representa un award (premio).
+    """
+    icon = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
+    price = models.IntegerField()
+
+    """
+    El ToString devuelve el titulo del premio.
+    """
+
+    def __str__(self):
+        return self.title
