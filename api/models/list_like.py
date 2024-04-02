01429396291e1df17c8c3f766760d273d17c4a88
Mario <marioperdiguero37@gmail.com>
2024-04-02 17:43:04 +0200

    🗃️ Add ORM models

---
+++ api/models/list_like.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class ListLike(models.Model):
+    """
+    Modelo que representa un "me gusta" en una lista.
+    """
+    user = models.ForeignKey('User', on_delete=models.DO_NOTHING)
+    list = models.ForeignKey('List', on_delete=models.DO_NOTHING)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.list.name}'
