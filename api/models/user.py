4c2fe98141fc953c08d673adee2f11b05fa47099
Mario <marioperdiguero37@gmail.com>
2024-04-02 17:00:48 +0200

    🗃️ Update User model

--- api/models/user.py
+++ api/models/user.py
@@ -9,8 +9,9 @@ class User(AbstractBaseUser, PermissionsMixin, TimeStamped):
     Modelo que representa a un usuario
     """
     username = models.CharField(max_length=50, unique=True)
-    avatar = models.ForeignKey('Avatar', on_delete=models.CASCADE)
     share_code = models.CharField(max_length=20, unique=True)
+    avatar = models.ForeignKey('Avatar', on_delete=models.DO_NOTHING)
+    client = models.ForeignKey('Client', on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return self.username + ' (' + self.share_code + ')'
