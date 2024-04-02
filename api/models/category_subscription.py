b13a38bd108fdc75c61666f88fa4be662b7aaed4
Mario <marioperdiguero37@gmail.com>
2024-04-02 17:38:37 +0200

    🗃️ Add ORM models

---
+++ api/models/category_subscription.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class CategorySubscription(models.Model):
+    """
+    Modelo que representa una suscripción de usuario a una categoría.
+    """
+    user = models.ForeignKey('User', on_delete=models.DO_NOTHING)
+    category = models.ForeignKey('Category', on_delete=models.DO_NOTHING)
+    notification = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.user} - {self.category}'
