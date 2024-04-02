46084f56b5b552fd185e0456b97a5ab84cb19e28
Mario <marioperdiguero37@gmail.com>
2024-04-02 17:58:04 +0200

    🗃️ Update GoalType model

--- api/models/goal_type.py
+++ api/models/goal_type.py
@@ -5,16 +5,14 @@ class GoalType(models.Model):
     """
     Modelo que representa los tipos de las misiones
     """
+    TARGET_CHOICES = (
+        (1, 'Para todos'),
+        (2, 'Solo para creadores'),
+        (3, 'Solo para usuarios'),
+    )
+
     title = models.CharField(max_length=100, unique=True)
-    target = models.IntegerField()
-
-    """
-    Para quien esta dirigida (target):
-
-    - Para todos
-    - Solo para creadores
-    - Solo para usuarios
-    """
+    target = models.IntegerField(choices=TARGET_CHOICES)
 
     def __str__(self):
         return self.title
