00c5f9e4937a519b9c1e16f5b603e255223d9e61
Miguel Rico <mricofernandez@safareyes.es>
2024-04-05 17:30:08 +0200

    ✨ Create create_list function in list_service

--- api/services/list_service.py
+++ api/services/list_service.py
@@ -1,6 +1,14 @@
 from api.models import List
 
 
+def create_list(list_form):
+    """Función que crea una lista"""
+    if list_form.is_valid():
+        return list_form.save(commit=False)
+    else:
+        return None
+
+
 def get_list(share_code):
     """Función que devuelve el objeto "lista" al que pertenece el sharecode"""
     try:
