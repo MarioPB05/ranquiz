2cb0ef6703c643c6a318b6d4a21cc5b9e60ac0fb
Miguel Rico <mricofernandez@safareyes.es>
2024-04-05 17:06:22 +0200

    ✨ Create get_items function

--- api/services/item_service.py
+++ api/services/item_service.py
@@ -1,3 +1,7 @@
+from api.models import Item
+from api.services.list_service import get_list
+
+
 def create_item(item_form):
     """Función para crear un item"""
 
@@ -6,3 +10,18 @@ def create_item(item_form):
         return item_form.save(commit=False)
     else:
         return None
+
+
+def get_items(share_code):
+    """Función para obtener los items de una lista"""
+
+    # Obtenemos la lista
+    required_list = get_list(share_code)
+
+    if required_list is None:
+        return None
+
+    # Obtenemos los items de la lista
+    items = Item.objects.filter(list=required_list)
+
+    return items
