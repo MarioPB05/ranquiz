9503b2297fa29cb4a456dafc07cedf356e2e79da
Miguel Rico <mricofernandez@safareyes.es>
2024-04-05 18:51:18 +0200

    ✨ Create category_form

---
+++ api/forms/category_form.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.forms import ModelForm
+from api.models import Category
+
+
+def CreateCategoryForm(ModelForm):
+    """Formulario para crear una categoría."""
+
+    name = forms.CharField(max_length=100)
+    share_code = forms.CharField(max_length=20)
+
+    class Meta:
+        model = Category
+        fields = '__all__'
