19be6735af5d481e02b6679a776267c60f74ad2f
Miguel Rico <mricofernandez@safareyes.es>
2024-04-05 18:53:20 +0200

    ✨ Create get_all_categories function

--- api/services/category_service.py
+++ api/services/category_service.py
@@ -1,3 +1,6 @@
+from api.models import Category
+
+
 def create_category(category_form):
     """Función para crear una categoría"""
 
@@ -6,3 +9,12 @@ def create_category(category_form):
         return category_form.save(commit=False)
     else:
         return None
+
+
+def get_all_categories():
+    """Función para obtener todas las categorías"""
+
+    # Obtenemos todas las categorías
+    categories = Category.objects.all()
+
+    return categories
