2f5bc62fa09ed30edc966dfadd3f93768aafcdb1
Mario <marioperdiguero37@gmail.com>
2024-04-05 20:32:40 +0200

    ✨ Add controller to obtain list types

--- api/routing.py
+++ api/routing.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
+from api.controllers.list_controller import get_list_types
 from api.controllers.shop_controller import highlight_calculator
 
 urlpatterns = [
     path('shop/highlight/calculator', highlight_calculator, name='api_shop_highlight_calculator'),
+    path('list/types', get_list_types, name='api_list_types'),
 ]
