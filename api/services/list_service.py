71e5d81dd1df0e191de9de4f15b67e7325dd34c7
Miguel Rico <mricofernandez@safareyes.es>
2024-04-05 16:09:42 +0200

    ♻️ Refactor code

--- api/services/list_service.py
+++ api/services/list_service.py
@@ -4,7 +4,6 @@ from api.models import List
 def get_list(share_code):
     """Función que devuelve el objeto "lista" al que pertenece el sharecode"""
     try:
-        list_result = List.objects.get(share_code=share_code)
-        return list_result
+        return List.objects.get(share_code=share_code)
     except List.DoesNotExist:
         return None
