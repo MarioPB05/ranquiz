f4ea6ce096dff137718facf68b8b34cfa6bbff2a
Mario <marioperdiguero37@gmail.com>
2024-04-05 18:45:21 +0200

    ✨ Implements user registration form

--- static/scripts/pages/register.js
+++ static/scripts/pages/register.js
@@ -1,8 +1,32 @@
-import {removePageLoader} from "/static/assets/js/ranquiz/utils.js";
+import {removePageLoader, initializeFlatpickr} from "/static/assets/js/ranquiz/utils.js";
+
+/**
+ * Esta función se encarga de cargar la lista de países en el select2
+ */
+function loadCountries() {
+    $.ajax({
+        type: 'GET',
+        url: 'https://cdn.jsdelivr.net/npm/world_countries_lists@latest/data/es/countries.json',
+        dataType: 'json',
+        success: function (response) {
+            $('#id_country').select2({
+                data: response.map(e => ({
+                    id: e.name,
+                    text: e.name
+                })),
+                dropdownAutoWidth: true
+            });
+        },
+        error: function () {
+        },
+        complete: function () {
+        }
+    });
+}
 
 $(document).ready(() => {
 
-    const element = document.querySelector("#kt_stepper_example_basic");
+    const element = document.querySelector("#register_stepper");
     const stepper = new KTStepper(element);
 
     stepper.on("kt.stepper.next", function (stepper) {
@@ -12,5 +36,9 @@ $(document).ready(() => {
     stepper.on("kt.stepper.previous", function (stepper) {
         stepper.goPrevious();
     });
+
+    initializeFlatpickr('#id_birthdate');
+    loadCountries();
+
     removePageLoader();
 });
