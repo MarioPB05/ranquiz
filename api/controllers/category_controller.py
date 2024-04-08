2b4fb79117e51eae81163271fbd461059872a52a
Mario <marioperdiguero37@gmail.com>
2024-04-08 19:01:31 +0200

    ✨ Add endpoint to get all categories

--- api/controllers/category_controller.py
+++ api/controllers/category_controller.py
@@ -1,3 +1,6 @@
+import json
+
+from django.core.serializers import serialize
 from django.http import JsonResponse
 
 from api.services.category_service import get_all_categories, similarity
@@ -14,3 +17,9 @@ def validate_category(request, category_name):
             break
 
     return JsonResponse({'validate': not found_similar})
+
+
+def get_categories(request):
+    """Función para obtener todas las categorías"""
+    serialized_categories = serialize('json', get_all_categories())
+    return JsonResponse({'categories': json.loads(serialized_categories)})
--- api/routing.py
+++ api/routing.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from api.controllers.category_controller import validate_category
+from api.controllers.category_controller import validate_category, get_categories
 from api.controllers.list_controller import get_list_types
 from api.controllers.shop_controller import highlight_calculator
 
 urlpatterns = [
     path('shop/highlight/calculator', highlight_calculator, name='api_shop_highlight_calculator'),
     path('list/types', get_list_types, name='api_list_types'),
+    path('category/', get_categories, name='api_categories'),
     path('category/validate/<str:category_name>', validate_category, name='api_category_validator')
 ]
