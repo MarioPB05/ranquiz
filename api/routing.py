0b75bbb88460df4ed672ef315a947c9507296ed0
Mario <marioperdiguero37@gmail.com>
2024-04-08 19:14:38 +0200

    🐛 Fix endpoint to create new category

--- api/routing.py
+++ api/routing.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from api.controllers.category_controller import validate_category, get_categories
+from api.controllers.category_controller import validate_category, get_categories, add_category
 from api.controllers.list_controller import get_list_types
 from api.controllers.shop_controller import highlight_calculator
 
@@ -8,5 +8,6 @@ urlpatterns = [
     path('shop/highlight/calculator', highlight_calculator, name='api_shop_highlight_calculator'),
     path('list/types', get_list_types, name='api_list_types'),
     path('category/', get_categories, name='api_categories'),
+    path('category/create/', add_category, name='api_category_create'),
     path('category/validate/<str:category_name>', validate_category, name='api_category_validator')
 ]
