0aeb189d0f7b22af15fe75723e80c82b5dd7a725
Mario <marioperdiguero37@gmail.com>
2024-04-09 18:41:17 +0200

    ♻️ Refactor forms, and fix errors

--- api/forms/list_form.py
+++ api/forms/list_form.py
@@ -6,13 +6,9 @@ from api.models import List
 class CreateListForm(ModelForm):
     """Formulario para crear una lista"""
 
-    owner = forms.ModelChoiceField(queryset=List.objects.all())
-    share_code = forms.CharField(max_length=20)
     name = forms.CharField(max_length=100)
-    public = forms.BooleanField(required=False)
     image = forms.ImageField(required=False)
-    type = forms.ChoiceField(choices=List.TYPE_CHOICES)
 
     class Meta:
         model = List
-        fields = '__all__'
+        fields = ['name', 'image']
