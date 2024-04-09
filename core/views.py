e393d612bd342ea0cef791b2bdf0234a067bc6ea
Mario <marioperdiguero37@gmail.com>
2024-04-09 19:01:18 +0200

    ♻️ Merge if statements

--- core/views.py
+++ core/views.py
@@ -29,33 +29,32 @@ def create_list_view(request):
     list_form = create_list_form(request)
     item_form = create_item_form(request, prefix='template')
 
-    if request.method == 'POST':
-        if list_form.is_valid():
-            list_obj = create_list(list_form)
-            list_obj.owner = request.user
-            list_obj.public = True if request.POST.get('visibility') == 'public' else False
+    if request.method == 'POST' and list_form.is_valid():
+        list_obj = create_list(list_form)
+        list_obj.owner = request.user
+        list_obj.public = True if request.POST.get('visibility') == 'public' else False
 
-            items_prefix = request.POST.get('items_prefix').split(',')
-            categories_names = request.POST.get('categories').split(',')
+        items_prefix = request.POST.get('items_prefix').split(',')
+        categories_names = request.POST.get('categories').split(',')
 
-            if list_obj is not None and len(items_prefix) > 0:
-                list_obj.type = 0
-                list_obj.save()
+        if list_obj is not None and len(items_prefix) > 0:
+            list_obj.type = 0
+            list_obj.save()
 
-                for prefix in items_prefix:
-                    item_form = create_item_form(request, prefix=prefix)
-                    item = create_item(item_form)
+            for prefix in items_prefix:
+                item_form = create_item_form(request, prefix=prefix)
+                item = create_item(item_form)
 
-                    if item_form.is_valid() and item is not None:
-                        item.list = list_obj
-                        item.save()
+                if item_form.is_valid() and item is not None:
+                    item.list = list_obj
+                    item.save()
 
-                if categories_names is not None:
-                    for category_name in categories_names:
-                        category = get_category(category_name=category_name)
+            if categories_names is not None:
+                for category_name in categories_names:
+                    category = get_category(category_name=category_name)
 
-                        if category is not None:
-                            set_category(list_obj, category)
+                    if category is not None:
+                        set_category(list_obj, category)
 
     return render(request, 'pages/manage_list.html', {
         'list_form': list_form,
