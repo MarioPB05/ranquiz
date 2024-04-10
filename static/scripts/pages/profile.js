524c930cb76c91ca4ff7e403131b4869295619cb
Mario <marioperdiguero37@gmail.com>
2024-04-10 19:39:45 +0200

    🚧 Creating user profile view

--- static/scripts/pages/profile.js
+++ static/scripts/pages/profile.js
@@ -1,3 +1,24 @@
-import { removePageLoader } from "/static/assets/js/ranquiz/utils.js";
+import { removePageLoader, toastMessage } from "/static/assets/js/ranquiz/utils.js";
 
-removePageLoader();
+/**
+ * Obtener cuando se ha hecho click en el botón de compartir cuenta
+ */
+function onShareAccount() {
+    toastMessage('success', '¡URL copiada al portapapeles!');
+}
+
+function onShareCode() {
+    toastMessage('success', '¡Código copiado al portapapeles!');
+}
+
+function pageLoaded() {
+    const clipboardShareProfile = new ClipboardJS($('#share_account')[0]);  // skipcq: JS-0125
+    const clipboardShareCode = new ClipboardJS($('#share_code')[0]);  // skipcq: JS-0125
+
+    clipboardShareProfile.on('success', onShareAccount);
+    clipboardShareCode.on('success', onShareCode);
+
+    removePageLoader();
+}
+
+$(document).ready(pageLoaded);
