b8f0de6c3b24b4f021817e0f4e0f91ae745a114f
David <dperezfernandez@safareyes.es>
2024-04-10 18:38:00 +0200

    ✨ Added some functionalities

--- static/scripts/pages/list_details.js
+++ static/scripts/pages/list_details.js
@@ -6,7 +6,8 @@ const templateComment = $("#template_comment");
 const templateAward = $("#template_award");
 let templateBuyableAward = $("#template_buyable_award");
 
-const commentsOnPage = [];
+let comments = [];
+let awards = [];
 
 let exampleComment = {
     "id": 1,
@@ -29,8 +30,6 @@ let exampleComment = {
     ]
 };
 
-let awards = [];
-
 /**
  * Obtiene los premios de la base de datos y los añade a la lista de premios en el comentario plantilla
  */
@@ -39,16 +38,16 @@ function getAwards() {
     awards = [
         {
             "id": 1,
-            "name": "Legendario",
-            "icon": "bi-trophy-fill",
+            "title": "Legendario",
+            "icon": "bi bi-trophy-fill",
             "color": "orange",
             "price": 10
         },
 
         {
             "id": 2,
-            "name": "Good",
-            "icon": "bi-ui-checks-grid",
+            "title": "Good",
+            "icon": "bi bi-ui-checks-grid",
             "color": "#23B0FF",
             "price": 5
         }
@@ -59,13 +58,65 @@ function getAwards() {
         award_element.removeAttr("id");
         award_element.removeClass("d-none").addClass("d-flex");
 
-        award_element.find(".award_name").text(award.name);
+        award_element.find(".award_name").text(award.title);
         award_element.find(".award_icon").addClass(award.icon);
-        award_element.find("a").css("background-color", award.color);
+        award_element.find("div").css("background-color", award.color);
         award_element.find(".award_price").text(award.price);
+
+        award_element.attr("data-award-id", award.id);
 
         award_element.appendTo(templateComment.find(".menu"));
     });
+}
+
+/**
+ * Añade un premio a un comentario
+ * @param award_id
+ * @param comment
+ * @param new_award
+ */
+function addAwardToComment(award_id, comment, new_award = false) {
+    let award = awards.find(award => award.id === award_id);
+
+    if (!new_award && uploadAward(award_id, comment.data("comment-id")).state !== "success") {
+        return;
+    }
+
+    if (comment.find(`div.award[data-award-id=${award_id}]`).length > 0) {
+        comment.find(`div.award[data-award-id=${award_id}]`).find(".award_amount").text(parseInt(comment.find(`div.award[data-award-id=${award_id}]`).find(".award_amount").text()) + 1);
+        console.log(comment.find(`div.award[data-award-id=${award_id}]`));
+
+    }else {
+        let award_element = templateAward.clone();
+        award_element.removeAttr("id");
+        award_element.removeClass("d-none").addClass("d-flex");
+
+        award_element.find(".award_icon").addClass(award.icon);
+        award_element.find(".award_name").text(award.title);
+        award_element.find(".award_amount").text(1);
+        award_element.css("background-color", award.color);
+
+        award_element.attr("data-award-id", award_id);
+        award_element.removeClass("d-none").addClass("d-flex");
+
+        award_element.appendTo(comment.find(".award_container"));
+        console.log(comment.find(".award_container"));
+    }
+}
+
+/**
+ * Sube un premio a la base de datos
+ * @param id_award
+ * @param id_comment
+ * @returns {{reason: string, state: string, id_award}}
+ */
+function uploadAward(id_award, id_comment) {
+    // TODO: Subir premio a la base de datos
+    return {
+        "state": "success",
+        "reason": "Premio subido correctamente",
+        id_award
+    }
 }
 
 /**
@@ -87,16 +138,34 @@ function addComment(comment) {
 
     element.find(".comment_content").text(content);
     element.find(".author_name").text(author_name);
-    // TODO: Add author avatar
+    element.find(".author_avatar").attr("src", author_avatar);
     element.find(".comment_date").text(formatElapsedTime(date));
 
-    element.find("#award_comment_first").attr("id", comment_temp_id);
     element.find('[data-kt-menu]').each(function () {
         const menu = new KTMenu($(this)[0]);
     });
 
+    if (awards) {
+        $.each(awards, function (index, award) {
+            addAwardToComment(award.id_award, element);
+        });
+    }
+
     commentsOnPage.push(comment);
     element.appendTo("#comments_container");
+    actualizeCommentCounter();
+}
+
+/**
+ * Obtiene los comentarios de la base de datos y los añade
+ */
+function getComments() {
+    // TODO: Obtener comentarios de la base de datos
+    let result = [exampleComment];
+
+    $.each(result, function (index, comment) {
+        addComment(comment);
+    });
 }
 
 /**
@@ -105,7 +174,12 @@ function addComment(comment) {
  * @returns {*}
  */
 function uploadComment(comment) {
+    // TODO: Subir comentario a la base de datos
     return exampleComment;
+}
+
+function actualizeCommentCounter() {
+    $("#comment_counter").text(commentsOnPage.length);
 }
 
 /**
@@ -127,7 +201,17 @@ function writeComment() {
 
 $(document).ready(function () {
     getAwards();
-    addComment(exampleComment);
+    getComments();
 
     $("#write_comment").click(writeComment);
+
+     $("#comments_container").on("click", ".buyable_award", function () {
+        let award_id = $(this).data("award-id");
+        let comment = $(this).parent().parent().parent().parent();
+
+        console.log(award_id);
+        console.log(comment);
+
+        addAwardToComment(award_id, comment, true);
+    });
 });
