eb253cb4306b1a0e09e51d1371b3f7d9f9d4e9dd
David <dperezfernandez@safareyes.es>
2024-04-12 19:32:55 +0200

    ✨ Added validation for a transaction

--- api/services/transaction_service.py
+++ api/services/transaction_service.py
@@ -1,8 +1,24 @@
-from api.models import UserTransaction
+from api.models import UserTransaction, User
 
 
 def do_transaction(user, value, details):
     """Realiza una transacción de monedas a un usuario"""
+    if not validate_transaction(user, value):
+        return None
+
     transaction = UserTransaction(user=user, value=value, details=details)
     transaction.save()
+
+    user.money += value
     return id(transaction)
+
+
+def validate_transaction(user, value):
+    """Valida una transacción"""
+    if value >= 0:
+        return True
+
+    if user is not None and value is not None:
+        return User.money >= (-value)
+
+    return False
