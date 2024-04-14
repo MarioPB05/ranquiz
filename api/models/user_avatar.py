bada773c3ba8c5f6deeb88689ff20053634f2168
david <dperezfernandez@safareyes.es>
2024-04-14 14:53:49 +0200

    ✨ New model UserAvatar

---
+++ api/models/user_avatar.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+from api.models.model_template import ModelTemplate
+
+
+class UserAvatar(ModelTemplate):
+    """Modelo que guarda los avatares que ha comprado un usuario"""
+
+    user = models.ForeignKey('User', on_delete=models.DO_NOTHING)
+    avatar = models.ForeignKey('Avatar', on_delete=models.DO_NOTHING)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username + ' - ' + self.avatar.name
