8ffa31e051241ebdee838fcbcb3a4db8cc95170f
Mario <marioperdiguero37@gmail.com>
2024-04-15 20:08:55 +0200

    🎨 Organize imports

--- ranquiz/asgi.py
+++ ranquiz/asgi.py
@@ -9,9 +9,10 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 
-from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+
 from websockets.routing import websockets_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ranquiz.settings')
