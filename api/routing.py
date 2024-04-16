69eb8aeb7569e487dce94cdf6f097fcfc064e9bf
Mario <marioperdiguero37@gmail.com>
2024-04-16 16:14:53 +0200

    ✨ Add endpoint to query money and user's picture

--- api/routing.py
+++ api/routing.py
@@ -3,6 +3,7 @@ from django.urls import path
 from api.controllers.category_controller import validate_category, get_categories, add_category
 from api.controllers.list_controller import get_list_types
 from api.controllers.shop_controller import highlight_calculator, get_avatars, buy_a_avatar, equip_a_avatar
+from api.controllers.user_controller import get_user_data
 
 urlpatterns = [
     path('shop/highlight/calculator', highlight_calculator, name='api_shop_highlight_calculator'),
@@ -13,4 +14,5 @@ urlpatterns = [
     path('shop/avatar/', get_avatars, name='api_shop_avatars'),
     path('shop/avatar/<int:avatar_id>/buy', buy_a_avatar, name='api_shop_avatar_buy'),
     path('shop/avatar/<int:avatar_id>/equip', equip_a_avatar, name='api_shop_avatar_equip'),
+    path('user/', get_user_data, name='api_user'),
 ]
