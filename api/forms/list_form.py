5132cb8e3874b3a7063f2fb4e49ea004f5c92a6a
david <dperezfernandez@safareyes.es>
2024-04-17 13:34:53 +0200

    ♻️ Added instance to list form

--- api/forms/list_form.py
+++ api/forms/list_form.py
@@ -6,7 +6,18 @@ from api.models import List
 class CreateListForm(ModelForm):
     """Formulario para crear una lista"""
 
-    name = forms.CharField(max_length=100)
+    def __init__(self, *args, **kwargs):
+        instance = kwargs.get('instance')
+        super(CreateListForm, self).__init__(*args, **kwargs)
+        if instance:
+            # Si se proporciona una instancia, inicializa los campos con los valores de la instancia
+            self.fields['name'].initial = instance.name
+            self.fields['question'].initial = instance.question
+
+    name = forms.CharField(
+        max_length=70,
+       widget=forms.TextInput(attrs={'class': 'form-control text-black', 'id': 'id_name', 'maxlength': '70', 'value': '', 'required': True})
+    )
     image = forms.ImageField(required=False)
     question = forms.CharField(
         max_length=100,
