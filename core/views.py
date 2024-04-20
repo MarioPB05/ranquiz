e65042a5c4a1d1a421cbda09fa2a3e88b73f404d
Mario <mperdiguerobarrera@safareyes.es>
2024-04-20 12:29:29 +0200

    ♻️ Update imports order

--- core/views.py
+++ core/views.py
@@ -4,20 +4,26 @@ from django.contrib.auth import logout as django_logout
 from django.contrib.auth.decorators import login_required
 from django.http import Http404, HttpResponseForbidden
 from django.shortcuts import render, redirect, get_object_or_404
-from api.models import List
-from api.services.category_service import edit_list_categories
-from api.services.item_service import create_item_form, create_item, edit_list_items
-from api.services.list_service import create_list_form, create_list, get_list
-from django.http import Http404
-from django.shortcuts import render, redirect
 
-from api.models import ListCategory, ListFavorite, ListLike, ListAnswer
-from api.services.category_service import get_category
-from api.services.item_service import get_items
-from api.services.list_service import get_list
-from api.services.item_service import create_item_form, create_item
-from api.services.list_service import create_list_form, set_category, create_list
-from api.services.user_service import user_login, user_register, get_user
+from api.models import List, ListCategory, ListFavorite, ListLike, ListAnswer
+from api.services.category_service import edit_list_categories, get_category
+from api.services.item_service import (
+    create_item_form,
+    create_item,
+    edit_list_items,
+    get_items,
+)
+from api.services.list_service import (
+    create_list_form,
+    create_list,
+    get_list,
+    set_category,
+)
+from api.services.user_service import (
+    user_login,
+    user_register,
+    get_user,
+)
 from core.decorators.decorators import partial_login_required
 
 
