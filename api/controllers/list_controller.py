40ed1724255236a9292c2e2425d103483760617e
Jorge Arias <jariasmartin@safareyes.es>
2024-04-20 05:05:24 +0200

    ♻️ Refactor code to be easier

--- api/controllers/list_controller.py
+++ api/controllers/list_controller.py
@@ -19,15 +19,18 @@ def get_list_types(request):
     return JsonResponse({'types': result})
 
 
+from django.http import JsonResponse
+
+
 def like_list(request, share_code):
-    list_instance = get_object_or_404(List, share_code=share_code)
     is_liked = request.POST.get('is_liked') == 'true'  # Convertir la cadena en un booleano
 
     if is_liked:
-        # Si ya está "liked", eliminar el like
-        ListLike.objects.filter(user=request.user, list=list_instance).delete()
+        # Si ya está "liked", eliminar el like si existe
+        ListLike.objects.filter(user=request.user, list__share_code=share_code).delete()
         return JsonResponse({'message': 'Like eliminado exitosamente'})
     else:
         # Si no está "liked", agregar el like
+        list_instance, _ = List.objects.get_or_create(share_code=share_code)
         ListLike.objects.create(user=request.user, list=list_instance)
         return JsonResponse({'message': 'Like registrado exitosamente'})
