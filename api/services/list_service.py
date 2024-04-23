6a636ccec1eac9eb45059e89f5e73efa755debd1
david <dperezfernandez@safareyes.es>
2024-04-23 19:39:47 +0200

    ♻️ Refactor get list function

--- api/services/list_service.py
+++ api/services/list_service.py
@@ -1,7 +1,6 @@
-from django.db import connection
-
 from api.forms.list_form import CreateListForm
 from api.models import List, ListCategory
+from api.services.query_service import execute_query
 
 
 def create_list_form(request, instance=None):
@@ -30,26 +29,31 @@ def get_list(share_code):
 
 def get_lists_order_default(limit=None, page=1, search='', user=None):
     """Función que devuelve las listas públicas con filtros ordenadas por defecto"""
-    with connection.cursor() as cursor:
-        cursor.execute("""SELECT l.id, l.name, l.share_code, l.image,
-                            (SELECT IF(COUNT(sll.id) > 0, TRUE, FALSE)
-                             FROM api_listlike sll
-                             WHERE sll.list_id = l.id AND sll.user_id = %s) AS liked,
-                            (SELECT COUNT(sla.id)
-                             FROM api_listanswer sla
-                             WHERE sla.list_id = l.id) AS plays,
-                            (SELECT IF(COUNT(shl.id) > 0, TRUE, FALSE)
-                             FROM api_highlightedlist shl
-                             WHERE shl.list_id = l.id 
-                                AND shl.start_date <= CURDATE() AND shl.end_date >= CURDATE()) AS highlighted,
-                            au.username as owner_username, au.share_code as owner_share_code, aa.image as owner_avatar
-                        FROM api_list l
-                        JOIN ranquiz.api_user au on l.owner_id = au.id
-                        JOIN ranquiz.api_avatar aa on au.avatar_id = aa.id
-                        WHERE l.public = TRUE AND l.name LIKE %s
-                        LIMIT %s OFFSET %s;""", [user.id, f"%{search}%", limit, (page - 1) * limit])
+    query = """SELECT l.id, l.name, l.share_code, l.image,
+                    (SELECT IF(COUNT(sll.id) > 0, TRUE, FALSE)
+                     FROM api_listlike sll
+                     WHERE sll.list_id = l.id AND sll.user_id = %s) AS liked,
+                    (SELECT COUNT(sla.id)
+                     FROM api_listanswer sla
+                     WHERE sla.list_id = l.id) AS plays,
+                    IF(hl.id IS NOT NULL AND hl.start_date <= NOW() AND hl.end_date >= NOW(), TRUE, FALSE) 
+                    AS highlighted,
+                    au.username as owner_username, au.share_code as owner_share_code, aa.image as owner_avatar
+                FROM api_list l
+                JOIN ranquiz.api_user au on l.owner_id = au.id
+                JOIN ranquiz.api_avatar aa on au.avatar_id = aa.id
+                LEFT JOIN ranquiz.api_highlightedlist hl on l.id = hl.list_id
+                WHERE l.public = TRUE AND l.name LIKE %s
+                ORDER BY 
+                    CASE 
+                        WHEN hl.id IS NOT NULL AND hl.start_date <= NOW() AND hl.end_date >= NOW() 
+                        THEN hl.start_date 
+                    END DESC
+                LIMIT %s OFFSET %s;"""
 
-        return [dict(zip([col[0] for col in cursor.description], row)) for row in cursor.fetchall()]
+    params = [user.id if user else None, f"%{search}%", limit, (page - 1) * limit]
+
+    return execute_query(query, params)
 
 
 def set_category(list_obj, category):
