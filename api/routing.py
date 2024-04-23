ed1e8beb4c7c81a9f6df68c933b43415eb665a43
David <dperezfernandez@safareyes.es>
2024-04-23 17:55:36 +0200

    ✨ Added the get list route

--- api/routing.py
+++ api/routing.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
 from api.controllers.category_controller import validate_category, get_categories, add_category
-from api.controllers.list_controller import get_list_types
+from api.controllers.list_controller import get_list_types, get_lists_filtered
 from api.controllers.shop_controller import highlight_calculator, get_avatars, buy_a_avatar, equip_a_avatar
 from api.controllers.user_controller import get_user_data
 
 urlpatterns = [
     path('list/types', get_list_types, name='api_list_types'),
+    path('lists/', get_lists_filtered, name='api_lists'),
     path('category/', get_categories, name='api_categories'),
     path('category/create/', add_category, name='api_category_create'),
     path('category/validate/<str:category_name>', validate_category, name='api_category_validator'),
