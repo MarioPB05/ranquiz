d4fc7471dd07245cc13ffe8c936ae91ac3e64718
david <dperezfernandez@safareyes.es>
2024-04-24 10:44:57 +0200

    ♻️ Added decorator

--- api/controllers/list_controller.py
+++ api/controllers/list_controller.py
@@ -21,25 +21,24 @@ def get_list_types(request):
     return JsonResponse({'types': result})
 
 
+@require_GET
 def get_lists_filtered(request):
     """Controlador que devuelve las listas"""
     page = int(request.GET.get('page', '1'))
     limit = 30
     sort = request.GET.get('sort', 'default')
     search = request.GET.get('search', '')
-    lists = []
     result = []
 
     lists = get_lists(limit, page, search, request.user, sort)
 
     # TODO: Cambiar URL por la de list details
-    print(lists)
     for list_obj in lists:
         result.append({
             'id': list_obj['id'],
             'name': list_obj['name'],
             'image':  f"https://res.cloudinary.com/dhewpzvg9/{list_obj['image']}" if list_obj['image'] else None,
-            'url': request.build_absolute_uri(reverse('api_lists', args=[])),
+            'url': '/',
             'liked': list_obj['liked'],
             'plays': list_obj['plays'],
             'highlighted': list_obj['highlighted'],
