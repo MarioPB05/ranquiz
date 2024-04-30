c96f8d3ecabb4e81e8e4f5f390f5f41a3b92eb7d
david <dperezfernandez@safareyes.es>
2024-04-30 10:17:56 +0200

    ✨ Toggle follow and bell

--- static/scripts/pages/category_lists.js
+++ static/scripts/pages/category_lists.js
@@ -1,5 +1,7 @@
 import { removePageLoader, promiseAjax } from "/static/assets/js/ranquiz/utils.js";
 const followButton  = $("#follow_category");
+const bellButton = $("#follow_category_bell");
+const content = $("#list_container");
 
 /**
  * Función que cambia el botón de seguir a siguiendo y viceversa
@@ -7,20 +9,40 @@ const followButton  = $("#follow_category");
 function toggleFollowButton() {
 
     if (followButton.hasClass("btn-primary")) {
-        followButton.removeClass("btn-primary");
-        followButton.addClass("btn-outline-primary");
+        followButton.removeClass("btn-primary").addClass("btn-outline-primary");
         followButton.addClass("btn-outline");
         followButton.addClass("btn-active-primary");
-        followButton.text("Seguir")
-        followButton.blur();
+        followButton.text("Seguir");
     }else {
-        followButton.addClass("btn-primary");
-        followButton.removeClass("btn-outline-primary");
+        followButton.addClass("btn-primary").removeClass("btn-outline-primary");
         followButton.removeClass("btn-outline");
         followButton.removeClass("btn-active-primary");
-        followButton.text("Siguiendo")
-        followButton.blur();
+        followButton.text("Siguiendo");
     }
+
+    followButton.blur();
+}
+
+/**
+ * Función que cambia el botón de notificaciones a activado y viceversa
+ */
+function toggleBellButton() {
+    if (bellButton.hasClass("btn-primary")) {
+        bellButton.removeClass("btn-primary").addClass("btn-outline-primary");
+        bellButton.addClass("btn-outline");
+        bellButton.addClass("btn-active-primary");
+        bellButton.find("i").removeClass("bi-bell-fill").addClass("bi-bell-slash-fill");
+        bellButton.attr("title", "Activar notificaciones");
+    }else {
+        bellButton.addClass("btn-primary").removeClass("btn-outline-primary");
+        bellButton.removeClass("btn-outline");
+        bellButton.removeClass("btn-active-primary");
+        bellButton.find("i").removeClass("bi-bell-slash-fill").addClass("bi-bell-fill");
+        bellButton.attr("title", "Desactivar notificaciones");
+    }
+    
+    bellButton.tooltip('dispose').tooltip();
+    bellButton.blur();
 }
 
 
@@ -29,7 +51,26 @@ function toggleFollowButton() {
  */
 function onDocumentReady() {
     // Evento de cambio de botón de seguir
-    followButton.on("click", toggleFollowButton);
+    followButton.on("click", () => {
+        toggleFollowButton();
+
+        if (followButton.hasClass("btn-primary")) {
+            bellButton.removeClass("d-none");
+        }else {
+            bellButton.addClass("d-none");
+        }
+    });
+
+    // Evento de cambio de botón de notificaciones
+    bellButton.on("click", toggleBellButton);
+
+    // Evento de click en la lista
+    content.on("click", ".list", (event) => {
+       if (event.target.tagName === "A") return;
+
+        const url = $(event.currentTarget).attr("href");
+        if (url) window.location.href = url;
+    });
 
     removePageLoader();
 }
