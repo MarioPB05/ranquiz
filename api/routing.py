6bec762b340f7041fd7a87e27e0bfb232034766a
David <dperezfernandez@safareyes.es>
2024-04-30 15:31:46 +0200

    ✨ Created a route for item controller

--- api/routing.py
+++ api/routing.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from api.controllers.category_controller import validate_category, get_categories, add_category, get_categories_filtered
+from api.controllers.item_controller import get_current_items, get_all_items
 from api.controllers.list_controller import get_list_types, like_list, favorite_list, get_lists_filtered
 from api.controllers.social_controller import get_comments, create_and_return_comment, add_award_to_comment_function, \
     get_awards
@@ -18,6 +19,8 @@ urlpatterns = [
     path('list/<str:share_code>/comment/create', create_and_return_comment, name='api_new_comment'),
     path('list/<str:share_code>/comment/<int:comment_id>/awards', create_and_return_comment,
          name='get_awards_from_comment'),
+    path('list/<str:share_code>/item/', get_current_items, name='get_list_items'),
+    path('list/<str:share_code>/item/all', get_all_items, name='get_all_items'),
     path('list/<str:share_code>/comment/<int:comment_id>/add_award', add_award_to_comment_function,
          name='add_award_to_comment'),
     path('social/awards', get_awards, name='get_all_awards'),
