3077590922602966c0831697f3a5a9ba5f02a917
david <dperezfernandez@safareyes.es>
2024-05-01 12:25:52 +0200

    ✨ Created new functions for highlight

--- static/scripts/pages/profile/list.js
+++ static/scripts/pages/profile/list.js
@@ -1,6 +1,7 @@
 import {initializeFlatpickr, promiseAjax, toastMessage} from "/static/assets/js/ranquiz/utils.js";
 
 const highlight_modal = $('#highlight_modal');
+const highlight_submit = $('#highlight_submit');
 const flatpickrInstance = initializeFlatpickr("#range_date_highlight", 'range', moment().format('YYYY-MM-DD')); // skipcq: JS-0125
 
 function toggleVisibility(event) {
@@ -73,6 +74,7 @@ function updateHighlightPrice() {
             .then(response => {
                 $('#highlight_price').text(response.price);
                 toogleHighLightButtons();
+                highlight_submit.prop('disabled', false);
             })
             .catch(() => {
                 toastMessage('error', 'Ha ocurrido un error al calcular el precio del destacado')
@@ -80,8 +82,23 @@ function updateHighlightPrice() {
     }
 }
 
-function highlightList(event) {
-    highlight_modal.modal('show');
+function showHighlightModal(event) {
+    $("#highlight_list_name").text($(this).data('name'));
+    highlight_submit.data('share_code', $(this).data('share_code'));
+    highlight_submit.prop('disabled', true);
+    flatpickrInstance.clear();
+    $('#highlight_price').text('0');
+    toogleHighLightButtons();
+
+    const share_code = highlight_submit.data('share_code');
+
+    promiseAjax(`/api/shop/highlight/${share_code}/check`, 'GET').then(response => {
+        if (response.highlighted) {
+            toastMessage('warning', 'Esta lista ya está destacada');
+        }else {
+            highlight_modal.modal('show');
+        }
+    });
 
     event.stopPropagation();
 }
@@ -93,7 +110,7 @@ function redirectToList() {
 function loadEvents() {
     $('#list_container')
         .on('click', '.toggle_visibility_list', toggleVisibility)
-        .on('click', '.highlight_list', highlightList)
+        .on('click', '.highlight_list', showHighlightModal)
         .on('click', '.list_item', redirectToList);
 
     $("#range_date_highlight").on("change", updateHighlightPrice);
@@ -106,6 +123,30 @@ function loadEvents() {
 
         flatpickrInstance.setDate([startDate, endDate]);
         updateHighlightPrice();
+    });
+
+    highlight_submit.on("click", (event) => {
+        const dates = $("#range_date_highlight").val().split(" hasta ");
+        const share_code = highlight_submit.data('share_code');
+
+        if (dates.length === 2) {
+            highlight_submit.prop('disabled', true);
+
+            promiseAjax(`/api/shop/highlight/${share_code}?start_date=${dates[0]}&end_date=${dates[1]}`, 'GET')
+                .then((response) => {
+                    if (response.status === 'success') {
+                        toastMessage('success', response.message);
+                        highlight_modal.modal('hide');
+                    } else {
+                        toastMessage('error', response.message);
+                    }
+                    highlight_submit.prop('disabled', false);
+                })
+                .catch(() => {
+                    toastMessage('error', 'Ha ocurrido un error al destacar la lista');
+                    highlight_submit.prop('disabled', false);
+                });
+        }
     });
 }
 
