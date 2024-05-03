73c5d0e149a0108197b321f6be283382ad010a3e
David <dperezfernandez@safareyes.es>
2024-05-03 20:22:35 +0200

    🗃️ Alter a column from list answer

--- api/models/list_answer.py
+++ api/models/list_answer.py
@@ -9,7 +9,7 @@ class ListAnswer(ModelTemplate):
     list = models.ForeignKey('List', on_delete=models.DO_NOTHING)
     user = models.ForeignKey('User', on_delete=models.DO_NOTHING)
     start_date = models.DateTimeField(auto_now_add=True)
-    end_date = models.DateTimeField()
+    end_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.user.username + ' - ' + self.list.name
