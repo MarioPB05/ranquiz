4e0030abac253399b393e611931ce968a16213ae
Mario <marioperdiguero37@gmail.com>
2024-05-08 22:04:56 +0200

    ✨ Create enum for notification types and create get method

--- api/models/notification_type.py
+++ api/models/notification_type.py
@@ -1,6 +1,24 @@
+from enum import Enum
+
 from django.db import models
 
 from api.models.model_template import ModelTemplate
+
+
+class NotificationTypes(Enum):
+    NEW_LIST = 1
+    NEW_LIST_ON_CATEGORY = 2
+    NEW_LIST_OPTIONS = 3
+    NEW_LIST_LIKE = 4
+    NEW_LIST_COMMENT = 5
+    NEW_COMMENT_AWARD = 6
+    NEW_FOLLOWER = 7
+    NEW_LIST_FAVORITE = 8
+
+    @property
+    def object(self):
+        """Obtiene un tipo de notificación"""
+        return NotificationType.get(self)
 
 
 class NotificationType(ModelTemplate):
@@ -12,3 +30,8 @@ class NotificationType(ModelTemplate):
 
     def __str__(self):
         return self.title
+
+    @classmethod
+    def get(cls, notification_type: NotificationTypes):
+        """Obtiene un tipo de notificación"""
+        return cls.objects.get(id=notification_type.value)
