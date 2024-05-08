5664bb9dff4b0c1998b0ad067e4f1063c46256e4
Mario <marioperdiguero37@gmail.com>
2024-05-08 22:05:35 +0200

    🔥 Remove methods (Extracted to model class)

--- api/services/notification_service.py
+++ api/services/notification_service.py
@@ -1,29 +1,5 @@
-from asgiref.sync import async_to_sync
-from channels.layers import get_channel_layer
-
-from api.models import Notification, NotificationType
+from api.models import Notification
 from api.services.social_service import get_following
-
-
-def create_notification(target, notification_type, user, share_code):
-    """Crea una notificación"""
-    notification = Notification(
-        target=target,
-        user=user,
-        type=notification_type,
-        share_code=share_code
-    )
-
-    notification.save()
-
-    send_notification(user, notification)
-
-    return notification
-
-
-def get_notification_type(type_id):
-    """Obtiene un tipo de notificación por su ID"""
-    return NotificationType.objects.get(id=type_id)
 
 
 def get_notifications(user):
@@ -39,27 +15,3 @@ def get_notifications(user):
     )
 
     return own_notifications.union(following_notifications).order_by('-date')
-
-
-def send_notification(user, notification):
-    """Envía una notificación a un usuario o usuarios"""
-    channel_layer = get_channel_layer()
-
-    if notification.target == 1:
-        channel_name = user.share_code
-    elif notification.target == 2:
-        channel_name = f'followers_{user.share_code}'
-    else:
-        channel_name = 'global'
-
-    async_to_sync(channel_layer.group_send)(
-        channel_name,
-        {
-            'type': 'send_notification',
-            'target': notification.target,
-            'icon': notification.type.icon,
-            'title': notification.type.title,
-            'description': notification.type.description,
-            'share_code': notification.share_code
-        }
-    )
