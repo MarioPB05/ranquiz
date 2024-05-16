ec53f6df47be1efb92184adf60a7cceebf56c403
Mario <marioperdiguero37@gmail.com>
2024-05-16 16:16:30 +0200

    ✨ Set title/desc notify when share code is list

--- websockets/consumers.py
+++ websockets/consumers.py
@@ -3,7 +3,7 @@ import json
 from asgiref.sync import sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
 
-from api.models import User
+from api.models import User, List
 from api.services.social_service import get_following
 
 
@@ -192,10 +192,21 @@ class NotificationsConsumer(AsyncWebsocketConsumer):
             # Es una notificación para los seguidores, el dueño también está en el grupo, pero no la debe recibir
             return
 
-        if event['share_code'][0] == 'US':
+        if event['share_code'][0:2] == 'US':
             notification_user = await sync_to_async(User.get)(share_code=event['share_code'])
             title = event['title'].replace('[USUARIO]', notification_user.username)
             desc = event['description'].replace('[USUARIO]', notification_user.username)
+        elif event['share_code'][0:2] == 'LS':
+            list_obj = await sync_to_async(List.get)(share_code=event['share_code'])
+            list_owner_username = await sync_to_async(lambda ls: ls.owner.username)(list_obj)
+            list_owner_share_code = await sync_to_async(lambda ls: ls.owner.share_code)(list_obj)
+
+            title = event['title'].replace('[USUARIO]', list_owner_username)
+            desc = event['description'].replace('[USUARIO]', list_owner_username)
+
+            if event['target'] == 2 and self.scope['user'].share_code == list_owner_share_code:
+                # Es una notificación para los seguidores, el dueño también está en el grupo, pero no la debe recibir
+                return
         else:
             title = event['title']
             desc = event['description']
