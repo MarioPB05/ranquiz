11d1a1f61a8d894b99cd800d637d3310d00b8d9b
Miguel Rico <mricofernandez@safareyes.es>
2024-05-17 18:29:35 +0200

    🐛 fix redirections and follow functions

--- static/scripts/pages/profile/resume.js
+++ static/scripts/pages/profile/resume.js
@@ -1,16 +1,18 @@
 /*global $, share_code*/
 
-import { toggleListLike } from "/static/assets/js/ranquiz/utils.js";
+import {toggleCategoryFollow, toggleListLike, toggleUserFollow} from "/static/assets/js/ranquiz/utils.js";
 
 let page = 2;
 let isLoadingData = false;
+
+const kt_content_container = $('#kt_content_container')
 
 /**
  * Redirige a la lista seleccionada
  *
  * @param event
  */
-function redirectToList(event) {
+function redirectTo(event) {
     if (event.target.tagName === "A") return;
 
     const url = $(event.currentTarget).attr("href");
@@ -78,8 +80,58 @@ function infiniteScroll(scroll = false) {
  * Carga los eventos de la página
  */
 function loadEvents() {
-    $('.list').on('click', redirectToList);
-    $('.list_like').on('click', toggleListLike);
+
+    // Evento para dar like a una lista
+    kt_content_container.on("click", ".list_like", toggleListLike);
+
+    // Evento para seguir a un usuario
+    kt_content_container.on("click", ".user_follow", (event) => {
+        $(this).prop("disabled", true);
+        const icon = $(event.currentTarget).find('i');
+
+        icon.toggleClass("bi-person-plus-fill").toggleClass("bi-person-check-fill text-primary");
+
+        toggleUserFollow(event).then(() => {
+            $(this).prop("disabled", false);
+        }).catch(() => {
+            icon.toggleClass("bi-person-plus-fill").toggleClass("bi-person-check-fill text-primary");
+            $(this).prop("disabled", false);
+        });
+    });
+
+    kt_content_container.on("click", ".category_follow", (event) => {
+        $(this).prop("disabled", true);
+        const button = $(event.currentTarget);
+
+        if (button.hasClass("btn-primary")) {
+            button.removeClass("btn-primary").addClass("btn-outline-primary");
+            button.text("Seguir");
+            button.blur();
+        } else {
+            button.removeClass("btn-outline-primary").addClass("btn-primary");
+            button.text("Siguiendo");
+        }
+
+        toggleCategoryFollow(event).then(() => {
+            $(this).prop("disabled", false);
+        }).catch(() => {
+            const buttonFollow   = $(event.currentTarget);
+            buttonFollow.toggleClass("btn-primary").toggleClass("btn-outline-primary");
+            buttonFollow.text("Seguir");
+            buttonFollow.blur();
+            $(this).prop("disabled", false);
+        });
+    });
+
+    // Evento de clic en un elemento
+    kt_content_container.on("click", ".list, .category_element, .user", (event) => {
+        if (event.target.tagName === "A") return;
+
+
+        const url = $(event.currentTarget).attr("href");
+        if (url) window.location.href = url;
+    });
+
 
     $('.scroll').scroll(() => infiniteScroll());
     $('#list_arrow_right').on('click', () => infiniteScroll(true));
