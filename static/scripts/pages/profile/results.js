7a2726f8cf40ae4250dfacc9aa39141142a47cf6
David <dperezfernandez@safareyes.es>
2024-05-17 16:19:33 +0200

    🐛 Fix page issue

---
+++ static/scripts/pages/profile/results.js
@@ -0,0 +1,42 @@
+
+/**
+ * Obtiene la URL actual
+ *
+ * @returns {URL}
+ */
+function getCurrentURL() {
+    return new URL(baseURL);
+}
+
+/**
+ * Busca listas por el nombre de la lista sin quitar los filtros actuales
+ *
+ * @param event
+ */
+function searchList(event) {
+    const url = getCurrentURL();
+    const search = $('#search_input').val();
+
+    if (event.key && event.key !== 'Enter') return; // Si se presionó una tecla y no fue Enter, no se hace nada
+
+    if (!search) {
+        url.searchParams.delete('search');
+
+        window.location.href = url.toString();
+        return;
+    }
+
+    url.searchParams.delete('page')
+    url.searchParams.set('search', search);
+    window.location.href = `${url.toString()}`;
+}
+
+/**
+ * Función que se ejecuta cuando el documento está listo
+ */
+function onDocumentReady() {
+    $('#search_input').on('keypress', searchList);
+    $('#search_btn').on('click', searchList);
+}
+
+$(document).ready(onDocumentReady);
