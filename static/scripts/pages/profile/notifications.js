35354b575669bb51aed834d98edcad2764f2b649
David <dperezfernandez@safareyes.es>
2024-05-21 20:30:34 +0200

    ✨ New JS for notification

---
+++ static/scripts/pages/profile/notifications.js
@@ -0,0 +1,35 @@
+import {promiseAjax, toastMessage} from "/static/assets/js/ranquiz/utils.js";
+
+function clear_notifications() {
+    promiseAjax('/api/notification/clear', 'GET').then(response => {
+        if (response.status === 'success') {
+            toastMessage('success', 'Notificaciones limpiadas');
+
+            setTimeout(() => {
+                window.location.reload();
+            }, 1000);
+        }
+    });
+}
+
+function onDocumentReady() {
+    $('#clear_notifications').on("click", function () {
+        Swal.fire({
+            title: "¿Estás seguro?",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonClass: "btn btn-primary",
+            confirmButtonText: "Limpiar",
+            closeOnConfirm: false,
+            cancelButtonClass: "btn btn-secondary",
+            cancelButtonText: "Cancelar"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                clear_notifications();
+            }
+        });
+    });
+}
+
+
+$(document).ready(onDocumentReady);
