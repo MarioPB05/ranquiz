7b4004babe09996cbf21c46ab94f841f010925c0
david <dperezfernandez@safareyes.es>
2024-05-22 11:40:32 +0200

    ♻️ Changed clear notifications to show all

--- static/scripts/pages/profile/notifications.js
+++ static/scripts/pages/profile/notifications.js
@@ -1,41 +1,19 @@
-import {promiseAjax, toastMessage} from "/static/assets/js/ranquiz/utils.js";
-
-/**
- * Función que limpia las notificaciones
- */
-function clear_notifications() {
-    promiseAjax('/api/notification/clear', 'GET').then(response => {
-        if (response.status === 'success') {
-            toastMessage('success', 'Notificaciones limpiadas');
-
-            setTimeout(() => {
-                window.location.reload();
-            }, 1000);
-        }
-    });
-}
 
 /**
  * Función que se ejecuta cuando el documento está listo
  */
 function onDocumentReady() {
-    $('#clear_notifications').on("click",  () => {
-        Swal.fire({ // skipcq: JS-0125
-            title: "¿Estás seguro?",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonClass: "btn btn-primary",
-            confirmButtonText: "Limpiar",
-            closeOnConfirm: false,
-            cancelButtonClass: "btn btn-secondary",
-            cancelButtonText: "Cancelar"
-        }).then((result) => {
-            if (result.isConfirmed) {
-                clear_notifications();
-            }
-        });
+    if (window.location.href.includes('show_all=true')) {
+        $('#show_all').text('Ocultar notificaciones leídas');
+    }
+
+    $('#show_all').on("click",  () => {
+        if (window.location.href.includes('show_all=true')) {
+            window.location.href = `/user/${share_code}/?card=notifications`;  // skipcq: JS-0125
+        }else {
+            window.location.href = `/user/${share_code}/?card=notifications&show_all=true`;  // skipcq: JS-0125
+        }
     });
 }
-
 
 $(document).ready(onDocumentReady);
